933f70fb9fd8a9eddc3d360707683dced706b944
sgw8 <vasylmiskiv@gmail.com>
2022-04-11 20:13:13 +0300

    real time validation was added

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -6,8 +6,23 @@ import { useState } from 'react'
 
 export default function FeedbackForm() {
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState(' ')
 
-  const handleTextChange = e => setText(e.target.value)
+  const handleTextChange = e => {
+    if(text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if(text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be at least 10 characters')
+      setBtnDisabled(true)
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
+
+    setText(e.target.value)
+  }
 
   return (
     <Card>
@@ -21,8 +36,10 @@ export default function FeedbackForm() {
             placeholder='write a review'
             value={text}
           />
-          <Button type='submit'>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>Send</Button>
         </div>
+
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   )
